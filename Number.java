ae6ce92f56136895326de9628a1ce776468421e3
kburk1997 <kburk97@optonline.net>
2015-08-05 11:46:54 -0400

    Update Number.java

--- Number.java
+++ Number.java
@@ -5,4 +5,27 @@ import java.util.ArrayList;
 
 public class Number {
 
+     private int value; // Stores value of number
+     private ArrayList<Integer> divisors = new ArrayList<Integer>(); // List for divisors (used to check for prime numbers and amicable pairs)
+
+     public int getValue() {
+          return value;
+     }
+     
+     public void setValue(int val){
+           value = val;
+     }
+
+     public void setDivisors(){
+
+     }
+
+     public ArrayList<Integer> getDivisors(){
+
+     }
+
+     public boolean isPrime(){
+
+     }
+
 }
